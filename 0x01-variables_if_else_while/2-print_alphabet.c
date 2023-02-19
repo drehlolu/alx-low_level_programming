379ffd7dd3c670ce36996dc6d98f80c76e6249ab
drehlolu <aakintimiro@gmail.com>
2023-02-19 03:15:00 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
