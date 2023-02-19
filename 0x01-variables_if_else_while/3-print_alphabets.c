9cae4b568ae3d15e599dc7e5a61f05aa0432046a
drehlolu <aakintimiro@gmail.com>
2023-02-19 03:52:19 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-#include <unistd.h
+#include <unistd.h>
+
 /**
- * main - entry point 
+ * main - entry point
+ *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
-	int ch;
+int ch;
 
-	for (ch='a'; ch <= 'z'; ch++)
-	{
-		if (ch != 'e' && ch != 'q')
-		{
-			putchar(ch);
-		}
-		else
-		{
-			putchar('/n');
-		}
-		return (0);
-	}
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+else
+{
+putchar('\n');
+}
+}
+return (0);
+}
